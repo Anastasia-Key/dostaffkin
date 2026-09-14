cc233062a3f5e6f18e128618a5fc971090414290
Anastasia-Key <161369293+Anastasia-Key@users.noreply.github.com>
2026-09-14 20:59:56 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Order } from './pages/order/order';
 import { Track } from './pages/track/track';
- 
+
 export const routes: Routes = [
   {
     path: '',
